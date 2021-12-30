461889e636ea72419e954d9fca9f90d31f4c1c01
coderchomo <codermermarket@gmail.com>
2021-12-30 20:23:58 +0700

    playlist

---
+++ src/app/youtube-playlist/youtube-playlist.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {Song} from "../service/song";
+import {SongService} from "../service/song.service";
+import {Routes} from "@angular/router";
+
+@Component({
+  selector: 'app-youtube-playlist',
+  templateUrl: './youtube-playlist.component.html',
+  styleUrls: ['./youtube-playlist.component.css']
+})
+export class YoutubePlaylistComponent implements OnInit {
+  playlist: Song[] = [];
+
+  constructor(private songService: SongService) { }
+
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this.playlist=this.songService.playlist;
+  }
+}
+const routes: Routes=[
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent
+  }
+];
